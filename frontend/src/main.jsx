49da24420144d75fbc76f7c416a8375512109900
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-04-25 09:36:48 -0300

    feat(app): integrate UserLocationProvider at root level

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { UserLocationProvider } from "./context/userLocation.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <UserLocationProvider>
+      <App />
+    </UserLocationProvider>
+  </StrictMode>
+);
