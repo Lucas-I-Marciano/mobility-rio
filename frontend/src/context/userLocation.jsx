85ee1c91cab9b97108d241185ea7f6b53bf11ae3
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-04-25 09:34:16 -0300

    feat(location): add user location context and provider

---
+++ frontend/src/context/userLocation.jsx
@@ -0,0 +1,17 @@
+import { createContext, useContext, useState } from "react";
+
+export const UserLocationContext = createContext();
+
+export const useUserLocation = () => {
+  return useContext(UserLocationContext);
+};
+
+export const UserLocationProvider = ({ children }) => {
+  const [userLocation, setUserLocation] = useState(null);
+
+  return (
+    <UserLocationContext.Provider value={{ userLocation, setUserLocation }}>
+      {children}
+    </UserLocationContext.Provider>
+  );
+};
