36c0db9de1626cbd2ea434e77b05ed88b50ce5ff
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-25 13:20:48 -0300

    feat: Add Welcome page to routing in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import { ConfirmLocation } from "./pages/ConfirmLocation";
 import { Routes, Route } from "react-router"
+import { ConfirmLocation } from "./pages/ConfirmLocation";
+import { Welcome } from "./pages/Welcome";
+
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/confirm" element={<ConfirmLocation />} />
+        <Route path="/" element={<Welcome />} />
       </Routes>
 
     </>
