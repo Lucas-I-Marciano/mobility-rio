779701d6a97aa0553bdae5c5a9751b86fe72d94d
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-25 14:23:28 -0300

    feat(App): Add /bus route for ChoseBusStop component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router"
 import { ConfirmLocation } from "./pages/ConfirmLocation";
 import { Welcome } from "./pages/Welcome";
+import { ChoseBusStop } from "./pages/ChoseBusStop"
 
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/confirm" element={<ConfirmLocation />} />
         <Route path="/" element={<Welcome />} />
+        <Route path="/confirm" element={<ConfirmLocation />} />
+        <Route path="/bus" element={<ChoseBusStop />} />
       </Routes>
 
     </>
