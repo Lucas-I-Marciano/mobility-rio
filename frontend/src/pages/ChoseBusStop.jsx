5343152212ee9b32f07f6e2aacf24c83f1ee9fda
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-04-25 18:00:22 -0300

    feat(location): display form on confirm location click

--- frontend/src/pages/ChoseBusStop.jsx
+++ frontend/src/pages/ChoseBusStop.jsx
@@ -1,23 +1,51 @@
+import { useState } from "react";
 import { MapBusStop } from "../components/MapBusStop";
 import { useLocation } from "../context/location";
-import { LocateUser } from "../components/LocateUser"
+
+const MyForm = () => {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="">
+          Selecione a Linha <input className="bg-gray-200" type="text" />
+        </label>
+      </form>
+    </div>
+  );
+};
 
 export const ChoseBusStop = () => {
-    const { userLocation, busStopLocation } = useLocation();
-    return (
-        <>
-            <div className="flex flex-col items-center gap-1">
-                <p>Clique no ponto de ônibus mais próximo de você e confirme para enviarmos te avisarmos quando o próximo ônibus passará</p>
-                <p>Cada quadrado azul é um ponto de ônibus, aumente o zoom para uma precisão melhor</p>
-                <MapBusStop />
-                {userLocation == null ? null : (
-                    <button
-                        onClick={() => {
-                            console.log(busStopLocation);
-                        }}
-                        className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Confirmar</button>
-                )}
-            </div>
-        </>
-    );
+  const { userLocation, busStopLocation } = useLocation();
+  const [showForm, setShowForm] = useState(false);
+  return (
+    <>
+      <div className="flex flex-col items-center pb-5">
+        <p>
+          Clique no ponto de ônibus mais próximo de você e confirme para
+          enviarmos te avisarmos quando o próximo ônibus passará
+        </p>
+        <p>
+          Cada quadrado azul é um ponto de ônibus, aumente o zoom para uma
+          precisão melhor
+        </p>
+      </div>
+      <div className="content flex gap-5">
+        <div>
+          <MapBusStop />
+          {userLocation == null ? null : (
+            <button
+              onClick={() => {
+                console.log(busStopLocation);
+                setShowForm(true);
+              }}
+              className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            >
+              Confirmar
+            </button>
+          )}
+        </div>
+        {showForm ? <MyForm /> : null}
+      </div>
+    </>
+  );
 };
