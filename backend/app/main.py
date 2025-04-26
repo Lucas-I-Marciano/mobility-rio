bf179df7b55644d64271299c029cd82525483f64
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-04-26 17:49:31 -0300

    feat(app): integrate alert router and database initialization on startup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.api.routes import bus, map
+from app.api.routes import bus, map, alerts
+from app.db import create_all_table_and_db
+from app.db.user_alerts import UserAlert
 
 app = FastAPI()
 
@@ -15,6 +20,11 @@ app.add_middleware(
 
 app.include_router(bus.router)
 app.include_router(map.router)
+app.include_router(alerts.router)
+
+@app.on_event("startup")
+async def creating_on_startup():
+    create_all_table_and_db()
 
 @app.get("/")
 async def root():
