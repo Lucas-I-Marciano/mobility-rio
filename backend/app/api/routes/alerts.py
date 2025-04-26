f34b3ffb658569900c556d21c504e271cb2173e6
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-04-26 17:48:35 -0300

    feat(alert): add create and get-by-id endpoints for user alerts

---
+++ backend/app/api/routes/alerts.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends, HTTPException
+from typing import Annotated
+from sqlmodel import Session
+
+from app.schemas.user_alerts import UserAlertRead, UserAlertCreate
+from app.db.user_alerts import UserAlert
+from app.db import get_session
+
+router = APIRouter(prefix="/alerts")
+
+session_dependency = Annotated[Session, Depends(get_session)] # Help on database management
+
+@router.post("/", response_model=UserAlertRead)
+def create_user_alert(
+    *,
+    session: session_dependency,
+    alert_in: UserAlertCreate
+):
+    # Cria uma instância do modelo de banco de dados a partir do modelo de entrada da API
+    db_alert = UserAlert.model_validate(alert_in) # SQLModel v0.0.14+
+
+    session.add(db_alert)
+    session.commit()
+    session.refresh(db_alert) # Recarrega o objeto para obter id, created_at, updated_at
+    return db_alert
+
+@router.get("/user-alerts/{alert_id}", response_model=UserAlertRead)
+def read_user_alert(
+    *,
+    session: session_dependency,
+    alert_id: int
+):
+    alert = session.get(UserAlert, alert_id)
+    if not alert:
+        raise HTTPException(status_code=404, detail="User Alert not found")
+    return alert
