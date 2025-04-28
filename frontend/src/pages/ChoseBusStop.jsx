23b3552e70c6d49fd1cbf4bbf8e255874b32f5fd
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-28 20:12:32 -0300

    feat(Page): Display bus table when line is changed

--- frontend/src/pages/ChoseBusStop.jsx
+++ frontend/src/pages/ChoseBusStop.jsx
@@ -3,6 +3,18 @@ import { MapBusStop } from "../components/MapBusStop";
 import { useLocation as useReactRouterLocation } from "react-router";
 import { AlertForm } from "../components/AlertForm"; // Importa o novo formulário
 import { useLocation } from "../context/location"; // Para pegar busStopLocation
+import { BusTable } from "../components/BusTable";
+
+const mockData = [
+  { "ordem": "A72176", "latitude": "-22,95323", "longitude": "-43,18937", "datahora": "1706553587000", "velocidade": "9", "linha": "410", "distancia": 50, "approaching": true },
+  { "ordem": "A72028", "latitude": "-22,93798", "longitude": "-43,19041", "datahora": "1706553584000", "velocidade": "16", "linha": "117", "distancia": 50, "approaching": false },
+  { "ordem": "A72056", "latitude": "-22,92861", "longitude": "-43,37332", "datahora": "1706553581000", "velocidade": "14", "linha": "691", "distancia": 50, "approaching": true },
+  { "ordem": "A72149", "latitude": "-22,93106", "longitude": "-43,17814", "datahora": "1706553589000", "velocidade": "0", "linha": "507", "distancia": 50, "approaching": false },
+  { "ordem": "A72048", "latitude": "-22,90587", "longitude": "-43,19056", "datahora": "1706553587000", "velocidade": "3", "linha": "007", "distancia": 50, "approaching": true },
+  { "ordem": "A72002", "latitude": "-22,93995", "longitude": "-43,20013", "datahora": "1706553586000", "velocidade": "7", "linha": "422", "distancia": 50, "approaching": false },
+  { "ordem": "A72150", "latitude": "-22,89012", "longitude": "-43,2923", "datahora": "1706553589000", "velocidade": "1", "linha": "SN422", "distancia": 50, "approaching": true },
+  { "ordem": "C47701", "latitude": "-23,00754", "longitude": "-43,30336", "datahora": "1706553593000", "velocidade": "35", "linha": "863", "distancia": 50, "approaching": true }
+];
 
 export const ChoseBusStop = () => {
   const routerLocation = useReactRouterLocation();
@@ -11,6 +23,7 @@ export const ChoseBusStop = () => {
   // Pega o ponto de ônibus selecionado no mapa desta página
   const { busStopLocation } = useLocation();
   const [showForm, setShowForm] = useState(false);
+  const [line, setLine] = useState(null)
 
   // Se não houver localização confirmada da página anterior, talvez redirecionar ou mostrar erro
   if (!confirmedUserLocation) {
@@ -49,13 +62,13 @@ export const ChoseBusStop = () => {
         {/* Layout principal: Mapa à esquerda/em cima, Formulário à direita/embaixo */}
         <div className="flex flex-col lg:flex-row gap-5 p-3 items-start justify-center ">
           <div
-            className={`flex-shrink-0 w-full lg:w-1/2 ${showForm ? "lg:w-1/3" : "lg:w-5/2"} transition-all duration-300 ease-in-out `}
+            className={`flex-shrink-0 w-full ${showForm ? "lg:w-1/2" : "lg:w-5/2"} transition-all duration-300 ease-in-out `}
           >
             {" "}
             {/* Ajusta largura */}
             {/* Passa a localização confirmada para centralizar o mapa */}
 
-            <MapBusStop initialCenter={confirmedUserLocation} />
+            <MapBusStop initialCenter={confirmedUserLocation} moreLocations={line ? mockData : null} />
             <div className="mt-2 text-center">
               {busStopLocation &&
                 !showForm && ( // Mostra botão só se ponto selecionado E form não visível
@@ -87,10 +100,15 @@ export const ChoseBusStop = () => {
                 Detalhes do Alerta
               </h2>
               {/* Passa o ponto selecionado para o formulário */}
-              <AlertForm selectedBusStop={busStopLocation} />
+              <AlertForm selectedBusStop={busStopLocation} setLine={setLine} />
+
             </div>
           )}
+          {line ? <BusTable data={mockData} /> : null}
         </div>
+        {/* Tabela de Dados Mocados */}
+
+
       </div>
     </>
   );
