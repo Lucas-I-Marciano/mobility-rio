cb25bc451ab1f66d3a74e41f6b553b12a6098c51
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-04-29 21:44:42 -0300

    feat(app): integrate notification router

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ load_dotenv()
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.api.routes import bus, map, alerts
+from app.api.routes import bus, map, alerts, notifications
 from app.db import create_all_table_and_db
 from app.db.user_alerts import UserAlert
 
@@ -21,6 +21,7 @@ app.add_middleware(
 app.include_router(bus.router)
 app.include_router(map.router)
 app.include_router(alerts.router)
+app.include_router(notifications.router)
 
 @app.on_event("startup")
 async def creating_on_startup():
